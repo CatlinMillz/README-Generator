169a8b7a1c4398789367332bd43ab585313b8848
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2023-12-06 14:10:45 -0500

    oops did not need html and css

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
-// Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// Create an array of questions for user input
+// an array of questions for user input
 const questions = [
   {
     type: 'input',
@@ -42,14 +42,14 @@ const questions = [
   },
 ];
 
-// Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('README.md generated successfully!')
   );
 }
 
-// Create a function to initialize app
+// function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
     // Generate README content based on user input
